55d74d1d127a34fad601ea1c3453a8caabef03da
furxx2000 <furxx2000@gmail.com>
2021-03-19 11:46:26 +0800

    daily practices

--- 02-Fundamentals-Part-2/starter/Assignments.js
+++ 02-Fundamentals-Part-2/starter/Assignments.js
@@ -130,6 +130,19 @@ console.log(neighbors);
 
 
 
+// Coding Challenge #2
+const calcTip = function(bill){
+   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+let bills = [125, 555, 44];
+console.log(bills);
+
+let tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips);
+
+let total = [(bills[0] + tips[0]), (bills[1] + tips[1]), (bills[2] + tips[2])];
+console.log(total);
 
 
 
