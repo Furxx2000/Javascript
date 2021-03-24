d4df83c7027cfd2ebfd6be192dd220977be55b1d
furxx2000 <furxx2000@gmail.com>
2021-03-24 21:04:40 +0800

    daily practices

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,8 +1,24 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = '23';
-if (x === 23) console.log(23);
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
-const calcAge = birthYear => 2037 - birthYear;
-console.log(calcAge(1992));
+// 1. Understanding the problem
+// What is amplitude? Answer: difference between highest and lowest temp
+// How to compute max and min temp?
+// What's a sensor error? And what to do?
+
+// 2. Breaking-up into sub-problems
+// How to ignore errors?
+// Find max value in temp array.
+// Find min value in temp array.
+// Subtract min from max(amplitude) and return it.
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    if (temps[i] > max) max = temps[i];
+  }
+  console.log(max);
+};
+calcTempAmplitude([3, 7, 5]);
