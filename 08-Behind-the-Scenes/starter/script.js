3a2f5b19c04c410e8bb5ced38d3b3b37cd0c876b
furxx2000 <furxx2000@gmail.com>
2021-06-23 15:08:59 +0800

    學習 - 學習Destructuring array

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -113,21 +113,22 @@
 // addArrow(2, 5, 8);
 
 // Primitive type
-let lastName = 'William';
+let lastName = 'Williams';
 let oldLastName = lastName;
 lastName = 'Davis';
 console.log(lastName, oldLastName);
 
-// Reference type
+// Reference types
 const jessica = {
   firstName: 'Jessica',
   lastName: 'Williams',
   age: 27,
 };
+
 const marriedJessica = jessica;
 marriedJessica.lastName = 'Davis';
 console.log('Before marriage:', jessica);
-console.log('after marriage:', marriedJessica);
+console.log('After marriage:', marriedJessica);
 
 // Copying objects
 const jessica2 = {
@@ -142,6 +143,5 @@ jessicaCopy.lastName = 'Davis';
 
 jessicaCopy.family.push('Mary');
 jessicaCopy.family.push('John');
-
 console.log('Before marriage:', jessica2);
-console.log('after marriage:', jessicaCopy);
+console.log('After marriage:', jessicaCopy);
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
 const restaurant = {
@@ -12,18 +12,34 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+  // openingHours: {
+  //   thu: {
+  //     open: 12,
+  //     close: 22,
+  //   },
+  //   fri: {
+  //     open: 11,
+  //     close: 23,
+  //   },
+  //   sat: {
+  //     open: 0, // Open 24 hours
+  //     close: 24,
+  //   },
+  // },
 };
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+const temp = main;
+main = secondary;
+secondary = temp;
+console.log(main, secondary);
