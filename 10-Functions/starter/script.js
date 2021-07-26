98523443364613d84a7b1b8de042cb992b936395
Wen Danny <78590433+Furxx2000@users.noreply.github.com>
2021-07-26 11:38:12 +0800

    學習 - 學習Functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -294,17 +294,86 @@
 // poll2.displayResults2.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
 // poll2.displayResults2.call({ answers: [1, 5, 3, 9, 6, 1] });
 
-// ------------------Immediately Invoked Function Expressions(IIFE)--------------------
-const runOnce = function () {
-  console.log('This will never run again');
-};
-runOnce();
+// // ------------------Immediately Invoked Function Expressions(IIFE)--------------------
+// const runOnce = function () {
+//   console.log('This will never run again');
+// };
+// runOnce();
 
-// IIFE
-(function () {
-  console.log('This will never run again');
-})();
+// // IIFE
+// (function () {
+//   console.log('This will never run again');
+// })();
 
-// Arrow IIFE
+// // Arrow IIFE
 
-(() => console.log('This will alson never run again'))();
+// (() => console.log('This will alson never run again'))();
+
+// // ----------------------------Closures--------------------------------
+// const secureBooking = function () {
+//   let passengerCount = 0;
+
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// };
+
+// const booker = secureBooking();
+
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker);
+
+// // -------------------------More Closures Examples--------------------------
+// // Example 1
+// let f;
+// const g = function () {
+//   const a = 23;
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
+
+// const h = function () {
+//   const b = 777;
+//   f = function () {
+//     console.log(b * 2);
+//   };
+// };
+
+// g();
+// f();
+// console.dir(f);
+
+// // Re-assigning f function
+// h();
+// f();
+// console.dir(f);
+
+// // Example 2
+// const boardPassengers = function (n, wait) {
+//   const perGroup = n / 3;
+
+//   setTimeout(function () {
+//     console.log(`We are now boarding all ${n} passengers`);
+//     console.log(`There are 3 groups, each with ${perGroup} passengers`);
+//   }, wait * 1000);
+
+//   console.log(`Will start boarding in ${wait} seconds`);
+// };
+
+// boardPassengers(180, 5);
+
+// // -------------------------Coding Challenge 2 ---------------------------
+// (function () {
+//   const header = document.querySelector('h1');
+//   header.style.color = 'red';
+
+//   window.document.querySelector('body').addEventListener('click', () => {
+//     header.style.color = 'blue';
+//   });
+// })();
+
