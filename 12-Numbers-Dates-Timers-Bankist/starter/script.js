864f1ff8211ee358c6d8ad59070a75d314056ce4
Wen Danny <78590433+Furxx2000@users.noreply.github.com>
2021-07-30 18:26:16 +0800

    學習 - 學習Numbers

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -286,3 +286,27 @@ console.log(Number.isFinite(23 / 0));
 console.log(Number.isInteger(23));
 console.log(Number.isInteger(23.0));
 console.log(Number.isInteger(23 / 0));
+
+// -----------------------------------Math and rounding-------------------------------------
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** 1 / 3);
+
+console.log(Math.max(5, 18, 23, 11, 2));
+console.log(Math.max(5, 18, '23', 11, 2));
+console.log(Math.max(5, 18, '23px', 11, 2));
+
+console.log(Math.min(5, 18, 23, 11, 2));
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) =>
+  Math.trunc(Math.random() * (max - min) + 1) + min;
+
+// console.log(randomInt(5, 20));
+
+// Rounding Integers
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
