2b3c41868b4468ff67100bbdc558303418a13934
Wen Danny <78590433+Furxx2000@users.noreply.github.com>
2021-08-13 19:04:55 +0800

    學習 - 學習Ajax

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,7 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.eu/rest/v2/name/taiwan');
+request.send();
