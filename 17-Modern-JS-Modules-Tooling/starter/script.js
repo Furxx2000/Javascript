de1655ddddfdfbec238febdb969a0fabb0c29748
Wen Danny <78590433+Furxx2000@users.noreply.github.com>
2021-08-17 19:58:44 +0800

    學習 - Modern JavaScript

---
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,51 @@
+// Importing module
+// import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// addToCart('bread', 5);
+// console.log(price, tq);
+// console.log('Importing module');
+
+// import { addToCart } from './shoppingCart.js';
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+// import add, { cart, arr } from './shoppingCart.js';
+// add('pizza', 2);
+// add('bread', 5);
+// add('apple', 4);
+
+// console.log(cart, arr);
+// const ShoppingCart2 = (function () {
+//   const cart = [];
+//   const shippingCost = 10;
+//   const totalPrice = 237;
+//   const totalQuantity = 23;
+
+//   const addToCart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(`${quantity} ${product} added to cart`);
+//   };
+
+//   const orderStock = function (product, quantity) {
+//     console.log(`${quantity} ${product} ordered from supplier`);
+//   };
+
+//   return {
+//     addToCart,
+//     cart,
+//     totalPrice,
+//     totalQuantity,
+//   };
+// })();
+
+// ShoppingCart2.addToCart('apple', 4);
+// ShoppingCart2.addToCart('pizza', 2);
+// console.log(ShoppingCart2);
+// console.log(ShoppingCart2.shippingCost);
+// console.log(ShoppingCart2.cart);
+
+// Export
+// export.addToCart = function (product, quantity) {
+//         cart.push({ product, quantity });
+//         console.log(`${quantity} ${product} added to cart`);
+//       };
